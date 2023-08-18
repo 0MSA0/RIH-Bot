19d11094690012829fd2e636cd35dff24868e52f
Sebastian Adler <sadler1@stud.hs-offenburg.de>
2023-08-18 17:57:13 +0200

    gitignore, renamed gamelist.json,  added setup function in GameSelect.py

--- cogs/GameSelect.py
+++ cogs/GameSelect.py
@@ -1,8 +1,9 @@
 
 import discord
 import json
-from discord.ext import commands, tasks
+from discord.ext import commands
 
+# TODO: Use a database
 JSON_FILE = "gamelist.json"
 
 
@@ -20,3 +21,7 @@ class GameSelect(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
         self.gamelist = read_json()
+
+
+async def setup(bot):
+    await bot.add_cog(GameSelect(bot))
