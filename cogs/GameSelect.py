3106548170ffc2ba0eed67080bada135338f8aa1
Sebastian Adler <sadler1@stud.hs-offenburg.de>
2023-08-18 22:14:44 +0200

    Updated read_json to give a list not a dict anymore

--- cogs/GameSelect.py
+++ cogs/GameSelect.py
@@ -7,20 +7,20 @@ from discord.ext import commands
 JSON_FILE = "gamelist.json"
 
 
-def read_json() -> dict:
+def read_json() -> list[str]:
     """
-        Reads JSON file and saves everything into a dictionary
+        Reads JSON file and saves everything into a list
     """
     with open(JSON_FILE, 'r', encoding='utf-8') as f:
         json_dict = json.load(f)
-    print("Succesfully copied JSON into dict.")
+    print("Succesfully copied JSON into a list.")
     return json_dict
 
 
 class GameSelect(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
-        self.gamelist = read_json()
+        self.gamelist: list[str] = read_json()
 
 
 async def setup(bot):
